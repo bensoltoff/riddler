d9126b7c5f80005acc0e1748ea07c21e94c97d7c
bensoltoff <ben@bensoltoff.com>
2016-03-18 15:57:44 -0400

    Return round number as well

--- trenchcoat_man/money_guess.R
+++ trenchcoat_man/money_guess.R
@@ -25,18 +25,22 @@ guess_money <- function(actual, n_tries = 9, min_val = 1, max_val = 1000){
     ## if actual is higher than guess, change min_val to guess
     ## if actual is lower than guess, change max_val to guess
     if(actual == guess){
-      return(TRUE)
+      return(c(TRUE, i))
     } else if(actual > guess) {
       min_val <- guess
     } else if(actual < guess) {
       max_val <- guess
     }
     
-    i <- i + 1      # iterate to next round if guess was incorrect
+    # iterate to next round if guess was incorrect
+    # as long as this is not the last round
+    if(i != n_tries){
+      i <- i + 1
+    }
   }
   
   # at this point still have not guessed the money amount, so lose
-  return(FALSE)
+  return(c(FALSE, i))
 }
 
 
